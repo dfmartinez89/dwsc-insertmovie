d750b20ba2abee6350d5ecd71e4889c46e694843
Damian Ferro <dfmartinez8909@gmail.com>
2022-05-15 12:26:28 +0200

    first commit

---
+++ src/main/java/dwsc/proyecto/insertmovie/InsertMovieApplication.java
@@ -0,0 +1,13 @@
+package dwsc.proyecto.insertmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsertMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsertMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dwsc/proyecto/insertmovie/InsertMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package dwsc.proyecto.insertmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsertMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
