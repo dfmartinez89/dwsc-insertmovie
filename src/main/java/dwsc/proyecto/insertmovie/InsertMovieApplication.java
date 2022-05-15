5805ab6781a54b9a7c7ba269354bbadf088a6821
Damian Ferro <dfmartinez8909@gmail.com>
2022-05-15 12:38:54 +0200

    discovery client

--- src/main/java/dwsc/proyecto/insertmovie/InsertMovieApplication.java
+++ src/main/java/dwsc/proyecto/insertmovie/InsertMovieApplication.java
@@ -2,8 +2,10 @@ package dwsc.proyecto.insertmovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InsertMovieApplication {
 
 	public static void main(String[] args) {
